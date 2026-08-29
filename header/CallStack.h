09565390ac09a86aaaade187d517096334b82711
sushantchahar <sushantchahar91@gmail.com>
2026-08-29 12:31:59 +0530

    Introduce the model for variables and parameters in stack frame stack frame model

--- header/CallStack.h
+++ header/CallStack.h
@@ -3,10 +3,20 @@
 #include <string>
 #include <vector>
 
+struct Variable
+{
+    std::string VariableName;
+    std::string VariableType;
+    std::string VariableValue;
+};
+
 struct StackFrame
 {
     std::string FunctionName;
     int FunctionDepth = 0;
+
+    std::vector<Variable> Variables;
+    std::vector<Variable> Parameters;
 };
 
 class CallStack
@@ -15,8 +25,10 @@ private:
     std::vector<StackFrame> StackFrames;
 
 private:
-    void PushFrame(const StackFrame &stackframe);
+    void PushFrame(const StackFrame stackframe);
     void PopFrame();
+    void AddVariable(StackFrame &stackframe, const Variable variable);
+    void AddParameter(StackFrame &stackframe, const Variable variable);
 
 public:
     void EnterFunction(std::string FunctionName);
--- src/CallStack.cpp
+++ src/CallStack.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void CallStack::PushFrame(const StackFrame& stackframe)
+void CallStack::PushFrame(const StackFrame stackframe)
 {
     StackFrames.push_back(stackframe);
 }
@@ -18,12 +18,25 @@ void CallStack::EnterFunction(std::string FunctionName)
     StackFrame stackframe;
     stackframe.FunctionName = FunctionName;
     stackframe.FunctionDepth = StackFrames.size() + 1;
+    Variable variable;
+    AddVariable(stackframe, variable);
+    AddParameter(stackframe, variable);
     PushFrame(stackframe);
 }
 
 void CallStack::ExitFunction()
 {
     CallStack::PopFrame();
+}
+
+void CallStack::AddVariable(StackFrame &stackframe, const Variable variable)
+{
+    stackframe.Variables.push_back(variable);
+}
+
+void CallStack::AddParameter(StackFrame &stackframe, const Variable variable)
+{
+    stackframe.Parameters.push_back(variable);
 }
 
 void CallStack::ShowStackFrames()
